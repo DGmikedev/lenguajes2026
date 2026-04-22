12565b723716451cacd089414ed1d1f740c6e042
DGmike.dev <dgmike.dev@gmail.com>
2026-04-22 17:36:27 -0600

    0000009

--- Php/basicos/hardware/hardware.php
+++ Php/basicos/hardware/hardware.php
@@ -38,7 +38,7 @@ $parametros = [
     "Partición (es)" =>                ["df -h",                               "wmic logicaldisk get size, freespace, caption"],
     "Información sistema" =>           ["uname -a",                            "systeminfo"],
     "Red / IP" =>                      ["ip addr",                             "ipconfig /all"],
-    "MAC Address" =>                   ["ip link",                             "getmac"],
+    "MAC Address" =>                   ["ip link",                             "getmac /fo csv /nh"],
     "Procesos activos" =>              ["ps aux",                              "tasklist"],
     "Uso de CPU en tiempo real" =>     ["top",                                 "wmic cpu get loadpercentage"],
     "Uso de memoria en tiempo real" => ["free -h / top",                       "tasklist / systeminfo"],
@@ -62,7 +62,36 @@ function strToArray($str){
     return $valores;
 }
 
-function tableDisk($strDisk){
+// crea un atabla para la MAC address
+function tableMAC($arry){
+
+    $tabla = "<table   style=\"width: 90%; margin: left; text-align: left;\">
+            <thead>
+                <tr>
+                    <th style:\" width:40%;  text-align:left;\">Dirección Física</th>
+                    <th style:\" width:60%;  text-align:left;\">Nombre Transporte</th>
+
+                </tr>
+            </thead><tbody>";
+
+    foreach($arry as $clave => $valor)
+    {
+        $valorA = explode(",",$valor);
+        $tabla .= "<tr>
+            <td>".str_ireplace("\"","",$valorA[0])."</td>
+            <td>".str_ireplace("\"","",$valorA[1])."</td>
+
+            </tr>";
+    }
+
+    $tabla .= "</tbody></table>";
+
+    return $tabla;
+}
+
+
+// crea una tabla para las particiones del disco
+function tableParticion($strDisk){
 
     $arrT = [];
     $row_cap = explode("\n",execCommand("wmic logicaldisk get caption"));
@@ -84,23 +113,12 @@ function tableDisk($strDisk){
         $arrT[2][$indexSpace] = $row_freespace[$indexSpace];
     }
 
-/*
-    for($index = 0; $index < count($arrT); $index++)
-    {
-        for($j= 0; $j < count($arrT[$index]); $j++)
-        {
-            echo " --- " . $arrT[$index][$j] . "---- <br>";    
-        }
-        
-    }
-*/
-
-    $tabla = "<table>
+    $tabla = "<table   style=\"width: 60%; margin: left; text-align: left;\">
             <thead>
                 <tr>
-                    <th>Caption</th>
-                    <th>Size</th>
-                    <th>FreeSpace</th>
+                    <th style:\" width:20%;  text-align:left;\">Partición</th>
+                    <th style:\" width:40%;  text-align:left;\">Tamaño</th>
+                    <th style:\" width:40%;  text-align:left;\">Espacio libre</th>
 
                 </tr>
             </thead><tbody>";
@@ -120,99 +138,21 @@ function tableDisk($strDisk){
         
     }
 
-
     $tabla .= "</tbody></table>";
-    
-
-    // var_dump($arrT);
-/*
-    array(3) { 
-        [0]=> array(8) 
-          { [0]=> string(10) "Caption " 
-            [1]=> string(10) "C: " 
-            [2]=> string(10) "D: " 
-            [3]=> string(10) "E: " 
-            [4]=> string(10) "F: " 
-            [5]=> string(10) "G: " 
-            [6]=> string(1) " " 
-            [7]=> string(0) "" 
-          } 
-        [1]=> array(8) { 
-            [0]=> string(15) "Size " 
-            [1]=> string(15) "970084839424 " 
-            [2]=> string(15) "119225618432 " 
-            [3]=> string(15) "535805952 " 
-            [4]=> string(15) "28595712000 " 
-            [5]=> string(15) " " 
-            [6]=> string(1) " " 
-            [7]=> string(0) "" 
-          } 
-            [2]=> array(8) { 
-            [0]=> string(15) "FreeSpace " 
-            [1]=> string(15) "632471392256 " 
-            [2]=> string(15) "76806033408 " 
-            [3]=> string(15) "535769088 " 
-            [4]=> string(15) "19087413248 " 
-            [5]=> string(15) " " 
-            [6]=> string(1) " " 
-            [7]=> string(0) "" 
-        } } 
-        
-*/
-
-
-    //echo str_ireplace(" ","X",$strDisk[3]) . " largo: " . strlen($strDisk[3]);
-   /* 
-   $tmp = strToArray($strDisk);
-   $tmp[0] = str_ireplace(" ","X",$strDisk);
-   echo "strToArray <br>";
-   var_dump($tmp);
-   echo "<br>-----------";
-   echo count($tmp);
-   echo "<br>";
-    */
-
-
-
-   
-
-   
 
     return $tabla;
-    // var_dump($command);
-    // die("DIES DISCK");
 
-    /*
-    $disck_size = "wmic logicaldisk get size";
-    $disck_free = "wmic logicaldisk get freespace";
-    $disck_caption = "wmic logicaldisk get caption";
-
-    $Size      = strToArray(explode("\n", shell_exec($disck_size) ) );
-    $FreeSpace = strToArray(explode("\n", shell_exec($disck_free) ) );
-    $Caption   = strToArray(explode("\n", shell_exec($disck_caption) ) );
-
-      
-
-    print_r($Size);
-    echo "<br>";
-    print_r($FreeSpace);
-    echo "<br>";
-    print_r($Caption);
-*/
 }
 
 
 // crea un tabla con los valores de la memoria
-
 function tableRAM($valStrg){
-
-    $rows = strToArray($valStrg);
 
     $tabla = "<table>";
 
-    for($i = 1; $i < count($rows); $i++){
+    for($i = 1; $i < count($valStrg); $i++){
 
-        $tabla .= "<tr><td> Módulo $i <td> <td> Cap: ". $rows[$i]."</td></tr>";
+        $tabla .= "<tr><td> Módulo $i <td> <td> Cap: ". $valStrg[$i]."</td></tr>";
 
     }
 
@@ -234,10 +174,17 @@ function execCommandToArray($val){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.jsdelivr XQuitarXsX  .net/npm/@tailwindcss/browser@4"></script>
-    <title>server</title>
+    <script src="https://cdn.jsdelivr".".net/npm/@tailwindcss/browser@4"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@300;400;500;700&display=swap" rel="stylesheet"><title>server</title>
     <style>
-        body{ background-color:#F5F5F4; }
+        body{ background-color:#F5F5F4; 
+        font-family: 'JetBrains Mono', monospace;
+        font-size: 12px;
+        }
+
+
     </style>
     
 </head>
@@ -259,19 +206,28 @@ function execCommandToArray($val){
 
     <!-- Tabla -->
     <div class="flex-1 overflow-auto p-4">
-      <table>
+      <table   style="width: 90%; margin: auto; text-align: left;  border: 1px solid #F5F5F4;">
         
         <thead  style="background-color:#7BF1A8;">
           <tr>
-            <th style="width: 20%;">VARIABLE</th>
-            <th style="width: 80%;">SALIDA</th>
+            <th style="width: 30%;">VARIABLE</th>
+            <th style="width: 70%;">SALIDA</th>
           </tr>
         </thead>
 
         <tbody>
             <?php
-
+                
+                $fbg = 0;
+                
+                $cbg = "#E2E8F0;";
+                
                 foreach($parametros as $clave => $valor){ 
+                    
+                    $fbg % 2 == 0 ? $fbg = 1 : $fbg = 0; 
+                    
+                    $fbg == 0 ? $cbg = "#ECFDF5;" : $cbg = "white";
+
                     $value_return  = match ($clave){
                         "Información sistema" => "LINK - INFORMACION SISTEMAS",
 
@@ -285,15 +241,11 @@ function execCommandToArray($val){
                         "CPU Nombre",
                         "CPU Numero de nucleos",
                         "CPU #Procesadores logicos" => strToArray(explode("\n",execCommand($valor[1])))[1],
+                        "Partición (es)" => tableParticion($valor[1]), 
+                        "Memoria RAM"  => tableRAM( strToArray(execCommandToArray($valor[1])) ),  
 
+                        "MAC Address" => tableMAC(strToArray(explode("\n",execCommand($valor[1])))),
 
-                        "Partición (es)" => tableDisk($valor[1]), //getHDiscData($valor[1]),
-
-
-                        "Memoria RAM"  => tableRAM( execCommandToArray($valor[1]) ),  
-
-
-                        "MAC Address" ,
                         "Uso de CPU en tiempo real" ,
                         "Uso de memoria en tiempo real" ,
                         "Serial de disco" ,
@@ -302,8 +254,8 @@ function execCommandToArray($val){
                         "Usuarios conectados"  => execCommand($valor[1])
                     };    
                     echo "  
-                        <tr>
-                          <td>" . $clave ."</td>
+                        <tr style=\" background-color:$cbg;\">
+                          <td>" . $clave ." :</td>
                           <td>" . $value_return . "</td>
                         </tr>";   
                 }  
