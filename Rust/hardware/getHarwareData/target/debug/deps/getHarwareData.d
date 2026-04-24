4ec2b7cb80f1f20a84fa416641ed899fd8d11610
DGmike.dev <dgmike.dev@gmail.com>
2026-04-23 19:48:44 -0600

    0000010

---
+++ Rust/hardware/getHarwareData/target/debug/deps/getHarwareData.d
@@ -0,0 +1,5 @@
+C:\Users\El JEFE\Desktop\DESARROLLO\lenguajes\Rust\hardware\getHarwareData\target\debug\deps\getHarwareData.d: src\main.rs
+
+C:\Users\El JEFE\Desktop\DESARROLLO\lenguajes\Rust\hardware\getHarwareData\target\debug\deps\getHarwareData.exe: src\main.rs
+
+src\main.rs:
---
+++ Rust/hardware/getHarwareData/target/debug/getHarwareData.d
@@ -0,0 +1,1 @@
+C:\Users\El\ JEFE\Desktop\DESARROLLO\lenguajes\Rust\hardware\getHarwareData\target\debug\getHarwareData.exe: C:\Users\El\ JEFE\Desktop\DESARROLLO\lenguajes\Rust\hardware\getHarwareData\src\main.rs
