693678e77752941fd4e83a26a57292a2d76ee31a
DGmike.dev <dgmike.dev@gmail.com>
2026-05-05 15:06:15 -0600

    0000023

---
+++ PowerShell/CommonFunctions/OutType.psm1
@@ -0,0 +1,35 @@
+function Export-Data{
+    param (
+        [string]$OutType, 
+        [string]$OutPath,
+        [Object]$DataIn
+    )
+
+    # Select Type of exportation
+    if($OutType -eq "json"){
+
+        $DataIn | ConvertTo-Json -Depth 3 | Out-File -Encoding UTF8 "$OutPath.json"
+
+        # out to return 
+        Write-Host "1"
+
+    }elseif( $OutType -eq "txt" ){
+
+        $DataIn | Format-List * | Out-File -Encoding UTF8 "$OutPath.txt"
+
+        # out to return 
+        Write-Host "1"
+
+    }elseif($OutType -eq "cmd"){
+
+        # out to return 
+        Write-Host $DataIn
+
+    }else{
+
+        # out to return 
+        Write-Host "Parametros no validos"
+    }
+}
+
+Export-ModuleMember -Function Export-Data
