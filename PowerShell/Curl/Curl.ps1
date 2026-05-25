40f979a15558eb2beb487c3aaf077143eb878a23
DGmike.dev <dgmike.dev@gmail.com>
2026-05-25 14:24:42 -0600

    form v2

--- PowerShell/Curl/Curl.ps1
+++ PowerShell/Curl/Curl.ps1
@@ -6,6 +6,8 @@ $reader = New-Object System.Xml.XmlNodeReader $xaml
 
 $window = [Windows.Markup.XamlReader]::Load($reader)
 
+# Se Obtienen los valores del formulario
+
 $url = $window.FindName("TxtUrl")
 
 $btnSend = $window.FindName("BtnSend")
@@ -16,47 +18,78 @@ $BlocTextResponse = $window.FindName("BlocTextResponse")
 
 $BlocTextHeaders = $window.FindName("BlocTextHeaders")
 
+$BlocTextHeadersReturn = $window.FindName("BlocTextHeadersReturn")
+
+
 $btnSend.Add_Click({
+
+    $headers_r = ""
 
     try{
 
-        $Response = Invoke-WebRequest -Uri $url.Text
+        # Aplicando Curl o Invoke-WebRequest
+        $Response = Invoke-WebRequest -Uri $url.Text -ErrorAction Stop
 
+
+
+        # tranforma el json que llega y lo reconfigura para la visualización 
+        # Es para poner un estilo de prety de forma facíl y local 
         $json = $Response | ConvertFrom-JSON | ConvertTo-JSON -Depth 10
+        $BlocTextResponse.Text = $json
 
-        $head = $Response.Headers
-
-        $BlocTextHeaders.Text = (
-            $head | ForEach-Object{
-                Write-Host "$($_.Key) : $($_.Value)"
-            }
-        ) -join "`r`n"
         
 
+        
        
+
     }catch{
 
         $json = $_.Exception.Message
+        $BlocTextResponse.Text = $json
+
+        $headers_r = "=====================`n"
+        $headers_r += "ESTATUS CODE:`n"
+        $headers_r += "SIN EXITO EN LA REQUEST"
+        $headers_r += "`n=====================`n`n"
+        $BlocTextHeadersReturn.Text = $headers_r
 
     }
+
+    # Obteniendo las cabeceras de retorno
+    # Y se le da formato todo en una sola línea de data
+    # con saltos.
+        $head = $Response.Headers.GetEnumerator()
+       
+
+
+        
+ $headers_r = "=====================`n"
+        $headers_r += "ESTATUS CODE:`n"
+        $headers_r += "SIN EXITO EN LA REQUEST"
+        $headers_r += "`n=====================`n`n"
+        $BlocTextHeadersReturn.Text = $headers_r
+
+       
+
+        
+
+
+        foreach($header in $head){
+    
+            $Nombre = $header.Key
+            $Valor  = $header.Value
+
+            $headers_r += "HEADER : [ $Nombre ] `n"  
+            $headers_r += "VALOR : [ $Valor ]"
+            $headers_r += "`n=====================`n"
+
+        }
+
+        $BlocTextHeadersReturn.Text = $headers_r
   
-    $BlocTextResponse.Text = $json
+    
 
 })
 
 $window.ShowDialog()
 
-
-
-
-<#
-$LoginParameters = @{
-    Uri             = 'https://www.contoso.com/login/'
-    SessionVariable = 'Session'
-    Method          = 'POST'
-    Body            = @{
-        User     = 'jdoe'
-        Password = 'P@S$w0rd!'
-    }
-}
-#>
